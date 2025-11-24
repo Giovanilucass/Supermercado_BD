23ab1684627bc476101e650628c4c0d054f55fc2
Tiago Silveira <167130804+TgoSil@users.noreply.github.com>
2025-11-24 19:01:18 -0300

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -5,17 +5,20 @@ from rotas.funcionarios import funcionarios_blueprint
 from rotas.fornecedores import fornecedores_blueprint
 from rotas.produtos import produtos_blueprint
 from rotas.caixa import caixa_blueprint
+from rotas.authService import auth_blueprint
 
 app = Flask(__name__)
+app.secret_key = "chaveextremamentesecretanowaymuitosecreto:O"
 CORS(app, origins="*")
 
 @app.route("/", methods=["GET"])
 def get_server(): 
     return jsonify("Servidor existe"), 200
 app.register_blueprint(clientes_blueprint)
-app.register_blueprint(funcionarios_blueprint)
-app.register_blueprint(fornecedores_blueprint)
-app.register_blueprint(produtos_blueprint)
+# app.register_blueprint(funcionarios_blueprint)
+# app.register_blueprint(fornecedores_blueprint)
+# app.register_blueprint(produtos_blueprint)
+app.register_blueprint(auth_blueprint)
 app.register_blueprint(caixa_blueprint)
 app.run("0.0.0.0", port=8000, debug=False)
 
